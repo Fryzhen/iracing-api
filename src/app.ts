94a1bfcea7301d8ead55bfb66435a2b8ddaa5ec9
npas <noepasquer0@gmail.com>
2025-04-18 10:54:02 +0200

    try deploy nodeapp

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,8 @@
 import express from 'express';
 import routes from './routes/routes';
+require('dotenv').config();
 
 const app = express();
-
 app.use(express.json());
-
-// Routes
 app.use('/', routes);
-
-export default app;
+app.listen();
